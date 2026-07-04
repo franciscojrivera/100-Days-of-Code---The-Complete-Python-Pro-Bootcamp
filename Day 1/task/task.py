18f2f93a87c9cfe6040451edb52dec96ac6fa8fa
Francisco Rivera <frivera_123@live.com>
2026-07-04 16:23:39 -0700

    Day 1 Project: Band Name Generator

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,3 @@
-name = input("What is your name?")
-length = len(name)
-print(length)
+city = input("What the name of the city you grew up in?")
+pet = input("What's your pets name?")
+print("Your band name could be " + city + " " + pet)
