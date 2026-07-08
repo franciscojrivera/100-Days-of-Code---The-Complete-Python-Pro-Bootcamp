27d917e4ca171694781e239ea5bf18113c6f38e0
Francisco Rivera <frivera_123@live.com>
2026-07-07 21:44:23 -0700

    Line Conversions

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -4,3 +4,10 @@ print(type("12345"))
 print(type(12345))
 print(type(3.14))
 print(type(True))
+
+#print("Number of letters in your name: " + len(input("Enter your name")))
+
+name = input("Enter your name: ")
+length = len(name)
+print(length)
+
