5b60bba558d34ac2567d7ceaf45d9475a94cb615
Francisco Rivera <frivera_123@live.com>
2026-07-07 22:10:27 -0700

    Mathematical Operations

--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -5,9 +5,4 @@ print(type(12345))
 print(type(3.14))
 print(type(True))
 
-#print("Number of letters in your name: " + len(input("Enter your name")))
-
-name = input("Enter your name: ")
-length = len(name)
-print(length)
-
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
