b917984abe08009887428e48dfcb79f1d44ccd38
Francisco Rivera <frivera_123@live.com>
2026-07-13 14:00:17 -0700

    Pizza Menu Calculator

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,28 @@
 print("Welcome to Python Pizza Deliveries!")
+bill = 0
+pizza = ""
 size = input("What size pizza do you want? S, M or L: ")
+if size == "S":
+    pizza = size
+    bill += 15
+elif size == "M":
+    pizza = size
+    bill += 20
+elif size == "L":
+    pizza = size
+    bill += 25
+else:
+    print("Sorry, please enter S, M, or L")
+
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+if pepperoni == "Y":
+    if pizza == "S":
+        bill += 2
+    else:
+        bill += 3
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"Your total bill is ${bill}")
 
