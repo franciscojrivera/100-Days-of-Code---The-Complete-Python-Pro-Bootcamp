102b0d4040503678ac737d6d9a2850164b700d7d
Francisco Rivera <frivera_123@live.com>
2026-07-14 12:33:04 -0700

    Bank Roulette Exercise

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,10 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+# the way I did it
+random_integer = random.randint(0, 4)
+print(friends[random_integer])
+
+#2nd Option
+print(random.choice(friends))
