67dec00b9bc01172ab947e0ffc90879b20c26572
Francisco Rivera <frivera_123@live.com>
2026-07-18 17:00:16 -0700

    Hangman Game Project

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -59,6 +59,7 @@ word_list = ["aardvark", "baboon", "camel"]
 
 # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 #  Set 'lives' to equal 6.
+lives = 6
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -90,6 +91,8 @@ while not game_over:
 
     # TODO-2: - If guess is not a letter in the chosen_word, Then reduce 'lives' by 1.
     #  If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
 
     if "_" not in display:
         game_over = True
@@ -97,3 +100,4 @@ while not game_over:
 
     # TODO-3: - print the ASCII art from 'stages'
     #  that corresponds to the current number of 'lives' the user has remaining.
+    print(stages[lives])
