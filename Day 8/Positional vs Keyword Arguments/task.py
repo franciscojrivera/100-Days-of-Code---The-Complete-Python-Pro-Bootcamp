af93abc2d0eb9aa2f995fa55db657db2ad3023fd
Francisco Rivera <frivera_123@live.com>
2026-07-20 15:18:35 -0700

    Calculate Love Score Exercise

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -12,3 +12,38 @@ def greet_with_name(name, location):
 
 greet_with_name("Francisco", "Los Angeles")
 greet_with_name(location="Los Angeles", name="Francisco")
+
+#Love Calculator
+def calculate_love_score(name1 , name2):
+    true = ['t', 'r', 'u', 'e']
+    love = ['l', 'o', 'v', 'e']
+
+    true_count = 0
+    love_count = 0
+
+# Check true count for both names
+    for char in name1:
+        for char2 in true:
+            if char == char2:
+                true_count += 1
+
+    for char in name2:
+        for char2 in true:
+            if char == char2:
+                true_count += 1
+
+#Check love count for both names
+    for char in name1:
+        for char2 in love:
+            if char == char2:
+                love_count += 1
+
+    for char in name2:
+        for char2 in love:
+            if char == char2:
+                love_count += 1
+
+    total = str(true_count) + str(love_count)
+    print(total)
+
+calculate_love_score("Francisco Rivera", "Carolyn Kim")
