c0c73fc8731b14bbc4f63aae3514d7c1bbfa094f
Francisco Rivera <frivera_123@live.com>
2026-07-20 14:11:22 -0700

    Positional vs Keyword Arguments

--- Day 8/Positional vs Keyword Arguments/task.py
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -1,8 +1,14 @@
-# Functions with input
+# # Functions with input
+#
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}?")
+#
+#
+# greet_with_name("Jack Bauer")
 
-def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
+def greet_with_name(name, location):
+    print(f"Hello, {name}! You are at {location}")
 
-
-greet_with_name("Jack Bauer")
+greet_with_name("Francisco", "Los Angeles")
+greet_with_name(location="Los Angeles", name="Francisco")
