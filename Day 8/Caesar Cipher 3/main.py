1b975c39de71268cf1ece7642151d4a0e6efba18
Francisco Rivera <frivera_123@live.com>
2026-07-22 16:45:56 -0700

    Caesar Cipher

--- Day 8/Caesar Cipher 3/main.py
+++ Day 8/Caesar Cipher 3/main.py
@@ -1,5 +1,6 @@
 # TODO-1: Import and print the logo from art.py when the program starts.
-
+import art
+print(art.logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
@@ -8,25 +9,35 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
 
     for letter in original_text:
-        if encode_or_decode == "decode":
-            shift_amount *= -1
-
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position %= len(alphabet)
-        output_text += alphabet[shifted_position]
+        # check if character is in alphabet
+        if letter in alphabet:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+        else:
+            output_text += letter
     print(f"Here is the {encode_or_decode}d result: {output_text}")
 
 
 # TODO-3: Can you figure out a way to restart the cipher program?
 
+loop = True
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+while loop == True:
 
-caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+
+    input_text = input("Would you like to run again? y/n\n").lower()
+    if input_text == "n":
+        loop = False
 
 
 
