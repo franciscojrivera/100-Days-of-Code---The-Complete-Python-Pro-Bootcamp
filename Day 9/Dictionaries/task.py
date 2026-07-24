ee3537ee3afb369a55f3dd9e87477a74b36afb4f
Francisco Rivera <frivera_123@live.com>
2026-07-23 17:30:04 -0700

    Dictionaries Exercise

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,21 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
+}
+
+
+programming_dictionary["Test"] = "This is a test"
+
+# #Wipes dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+
+# Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer"
+
+print(programming_dictionary)
+
+# Loop through a dictionary
+for thing in programming_dictionary:
+    print(thing)
