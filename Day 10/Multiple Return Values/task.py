e506f72c228ab1fb8837bc67a35d00c9d6e8617a
Francisco Rivera <frivera_123@live.com>
2026-08-04 19:42:41 -0700

    Docstrings Exercise

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -4,4 +4,14 @@ def format_name(f_name, l_name):
     return f"{formated_f_name} {formated_l_name}"
 
 
-print(format_name("AnGEla", "YU"))
+#print(format_name(input("What is your first name?"), input("What is your last name?")))
+
+
+def is_leap_year(year):
+    # Write your code here.
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False
+
+is_leap_year(2020)
