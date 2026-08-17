36c3edbb26f4c8f8b7a3ab16a14630f1af5515b7
Francisco Rivera <frivera_123@live.com>
2026-08-16 22:36:00 -0700

    Namespaces and Scope Exercise

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,10 +1,10 @@
 enemies = 1
 
-
+#enemies will not change outside this loop
 def increase_enemies():
     enemies = 2
     print(f"enemies inside function: {enemies}")
 
-
+# will print 2 then 1
 increase_enemies()
 print(f"enemies outside function: {enemies}")
